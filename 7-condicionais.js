9be1f3da7cfe8e36c572ad418ea06137d8752011
AYR-TI <alyrodob@gmail.com>
2022-04-15 03:07:05 -0300

    Condicionais parte 2

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,14 +5,17 @@ const listaDeDestinos = new Array("Salvador", "São Paulo", "Rio de Janeiro");
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
+const idadeComprador = 14;
+const estaAcompanhada = false;
 
-const idadeComprador = 14
-
-if(idadeComprador >=18){
-    console.log("Comprador maior, venda feita")
-    listaDeDestinos.splice(1, 1);
+if (idadeComprador >= 18) {
+  console.log("Comprador maior, venda feita");
+  listaDeDestinos.splice(1, 1);
+} else if (estaAcompanhada) {
+  console.log("Menor acompanhado, venda feita");
+  listaDeDestinos.splice(1, 1);
 } else {
-    console.log('Menor de idade, Não posso vender')
+  console.log("Menor de idade e não está acompanhado, Não posso vender");
 }
 
 console.log("Destinos possíveis:");
